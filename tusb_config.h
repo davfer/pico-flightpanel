0d2f917bafaa18514cf493ed31ebf1e96d4f51db
davfer <paido2005@gmail.com>
2025-05-14 20:00:56 +0200

    working usb+dvi

--- tusb_config.h
+++ tusb_config.h
@@ -79,7 +79,7 @@
 #endif
 
 #ifndef CFG_TUSB_MEM_ALIGN
-#define CFG_TUSB_MEM_ALIGN          __attribute__ ((aligned(4)))
+#define CFG_TUSB_MEM_ALIGN        __attribute__ ((aligned(4)))
 #endif
 
 //--------------------------------------------------------------------
@@ -91,14 +91,14 @@
 #endif
 
 //------------- CLASS -------------//
-#define CFG_TUD_HID               1
 #define CFG_TUD_CDC               0
 #define CFG_TUD_MSC               0
+#define CFG_TUD_HID               1
 #define CFG_TUD_MIDI              0
 #define CFG_TUD_VENDOR            0
 
 // HID buffer size Should be sufficient to hold ID (if any) + Data
-#define CFG_TUD_HID_EP_BUFSIZE    16
+#define CFG_TUD_HID_EP_BUFSIZE    64
 
 #ifdef __cplusplus
  }
